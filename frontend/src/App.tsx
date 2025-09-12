f4ff95653fcc4431f4814d951c3d4cd629b57c1f
Anwar-kD <202311789@cegepsherbrooke.qc.ca>
2025-09-12 14:35:07 -0400

    anwar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,18 +7,7 @@ function App() {
   return (
     <>
      <div>
-<<<<<<< HEAD
-      Maher ben hdibi
-      Tommy  riviere     
-      bright ogbeiwi 
-      Anwar ourkid
-    
-=======
-      Je M'inspire
-      Anwar Oukrid
-      Tommy Lariviere
-      Bright Ogbeiwi (MrCreeperman147)
->>>>>>> 7436a7fc2d6f3e93cf17ee2f54a3d47a08efb3b7
+      Anwar ourkid  
      </div>
     </>
   )
