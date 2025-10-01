514d1614c8c1ece985986ccb113177d284ee811e
MrCreeperman147 <clement.ogbeiwi@gmail.com>
2025-10-01 09:19:50 -0400

    User Unit Testing
    incomplete and broken cuz branch is incomplete and broken

--- backend/tests/Feature/Auth/AuthenticationTest.php
+++ backend/tests/Feature/Auth/AuthenticationTest.php
@@ -3,7 +3,9 @@
 namespace Tests\Feature\Auth;
 
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class AuthenticationTest extends TestCase
@@ -12,18 +14,27 @@ class AuthenticationTest extends TestCase
 
     public function test_login_screen_can_be_rendered(): void
     {
-        $response = $this->get('/login');
+        $response = $this->get('/');
 
         $response->assertStatus(200);
     }
 
     public function test_users_can_authenticate_using_the_login_screen(): void
     {
-        $user = User::factory()->create();
 
-        $response = $this->post('/login', [
+        $user = User::create([
+            'name' => "Helpy",
+            'last_name' => "Fazbear",
+            'email' => "helpyfazbear@gmail.com",
+            'date_of_birth' => Carbon::parse("2002-06-10"),
+            'city' => "Wingen sur Moder",
+            'password' => Hash::make("Freddy"),
+        ]);
+
+
+        $response = $this->post('/', [
             'email' => $user->email,
-            'password' => 'password',
+            'password' => 'Freddy',
         ]);
 
         $this->assertAuthenticated();
@@ -32,11 +43,18 @@ class AuthenticationTest extends TestCase
 
     public function test_users_can_not_authenticate_with_invalid_password(): void
     {
-        $user = User::factory()->create();
+        $user = User::create([
+            'name' => "Helpy",
+            'last_name' => "Fazbear",
+            'email' => "helpyfazbear@gmail.com",
+            'date_of_birth' => Carbon::parse("2002-06-10"),
+            'city' => "Wingen sur Moder",
+            'password' => Hash::make("Freddy"),
+        ]);
 
-        $this->post('/login', [
+        $this->post('/', [
             'email' => $user->email,
-            'password' => 'wrong-password',
+            'password' => 'Fazbear',
         ]);
 
         $this->assertGuest();
@@ -44,7 +62,14 @@ class AuthenticationTest extends TestCase
 
     public function test_users_can_logout(): void
     {
-        $user = User::factory()->create();
+        $user = User::create([
+            'name' => "Helpy",
+            'last_name' => "Fazbear",
+            'email' => "helpyfazbear@gmail.com",
+            'date_of_birth' => Carbon::parse("2002-06-10"),
+            'city' => "Wingen sur Moder",
+            'password' => Hash::make("Freddy"),
+        ]);
 
         $response = $this->actingAs($user)->post('/logout');
 
