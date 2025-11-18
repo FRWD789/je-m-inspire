bf65ea9056713cc9ed0685aa7035c9ab48bbc2e7
Tommy Lariviere <101297711+tomlol128@users.noreply.github.com>
2025-11-17 22:47:16 -0500

    Update AuthController.php

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -321,8 +321,7 @@ class AuthController extends Controller
 
             $user->load('roles');
 
-            Notification::route('mail', config('app.admin_email'))
-            ->notify(new ProfessionalApplicationReceivedNotification(
+            Notification::route('mail', config('app.admin_email'))->notify(new ProfessionalApplicationReceivedNotification(
                 $user,
                 $validated['motivation_letter']
             ));
