6e8be98b2f2b25a24d0a8cbd5207e0f6cde1dcd6
Tommy Lariviere <101297711+tomlol128@users.noreply.github.com>
2025-11-24 12:31:01 -0500

    Update EventManagementController.php

--- backend/app/Http/Controllers/EventManagementController.php
+++ backend/app/Http/Controllers/EventManagementController.php
@@ -174,7 +174,7 @@ class EventManagementController extends Controller
 
             // Vérifier que l'utilisateur est bien le créateur de l'événement
             $event = Event::where('id', $eventId)
-                ->where('user_id', $user->id)
+                ->where('id', $user->id)
                 ->first();
 
             if (!$event) {
