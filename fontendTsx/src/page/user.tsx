e31be9749242185b5ed3da0696ed2ca567483286
Tommy Lariviere <101297711+tomlol128@users.noreply.github.com>
2025-11-24 20:42:25 -0500

    Update user.tsx

--- fontendTsx/src/page/user.tsx
+++ fontendTsx/src/page/user.tsx
@@ -73,7 +73,7 @@ export default function User() {
     if (user?.profile.profile_picture) {
       setPreview(user.profile.profile_picture);
     } else {
-      setPreview("/default-avatar.png");
+      setPreview("/assets/img/default-avatar.png");
     }
   }, [user]);
 
